7d9e41be40410001a52f8f89be2bcfbe158d2bdc
bikkusah <87112266+bikkusah@users.noreply.github.com>
2024-05-11 15:19:14 +0530

    Add files via upload

---
+++ FunRestController.java
@@ -0,0 +1,28 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	@Value("${coach.name}")
+	private String coachName;
+	@Value("${team.name}")
+	private String teamName;
+	@GetMapping("/")
+	public String sayHelloWord() {
+		return "hello world ..universe";
+	}
+	@GetMapping("/hi")
+	public String hi() {
+		return "hiiiii";
+	}
+	@GetMapping("/team")
+	public String ha() {
+		
+		return "hahaha. meow moew .  " + coachName + " "+ teamName;
+	}
+	
+
+}
